13c50a53c2f23d84853b598e3f4c0cefb6cf3317
Alexander Swensen <alex.swensen@gmail.com>
2017-07-08 19:25:21 -0500

    Update with some refactored code

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,7 @@
 #include "FastLED.h"
-
+#include <EEPROM.h>
 // How many leds in your strip?
-#define NUM_LEDS 67
+#define NUM_LEDS 88
 
 // For led chips like Neopixels, which have a data line, ground, and power, you just
 // need to define DATA_PIN.  For led chipsets that are SPI based (four wires - data, clock,
@@ -9,28 +9,61 @@
 #define DATA_PIN 5
 #define CLOCK_PIN 13
 
+
+#define LED_TYPE    WS2812 // Any LEDS's that FastLED supports.
+#define COLOR_ORDER GRB // LED Order (If your colors are wrong, flip this around.)
+
 // Define the array of leds
 CRGB leds[NUM_LEDS];
-
+String incomingString = String("");   // for incoming serial data
+String MODE = String("");
+// Do some setup.
 void setup() {
-        Serial.begin(57600);
-        Serial.println("resetting");
-        LEDS.addLeds<WS2812, DATA_PIN, GRB>(leds, NUM_LEDS);
-        LEDS.setBrightness(50);
+  Serial.begin(57600);
+  LEDS.addLeds<LED_TYPE, DATA_PIN, COLOR_ORDER>(leds, NUM_LEDS);
+  LEDS.setBrightness(50);
+  Serial.println("Ready");
+}
+
+void setSetting(String input) {
+  MODE = input;
+}
+
+void readSerial() {
+
+  if (Serial.available() > 0) {
+    // read the incoming byte:
+    incomingString = Serial.readString();
+
+    // say what you got:
+    Serial.print("I received: ");
+    Serial.println(incomingString);
+    setSetting(incomingString);
+  }
 }
 
 void solidColor() {
-        // First slide the led in one direction
-        for (int i = 0; i < NUM_LEDS; i++) {
-                leds[i] = CRGB(20, 20, 20);
-        }
+  // First slide the led in one direction
+  for (int i = 0; i < NUM_LEDS; i++) {
+          leds[i] = CRGB(10, 100, 255);
+  }
 
-        FastLED.show();
-        // Serial.print(COLORS[x]);
-        // Serial.print('\n');
-        delay(200);
+  FastLED.show();
+  // Serial.print(COLORS[x]);
+  // Serial.print('\n');
+  delay(200);
 }
 void loop() {
-        solidColor();
-        Serial.print("x");
+  if (MODE.equals("test")) {
+    Serial.println("test worked");
+  }
+  if (MODE != "test") {
+    Serial.println("WTF");
+  }
+
+  Serial.print("current mode is: ");
+  Serial.println(MODE);
+  readSerial();
+  solidColor();
+  // Serial.print("x");
 }
