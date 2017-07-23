46f61605874149708a65a1f1854ca28e1a31457b
Alexander Swensen <alex.swensen@gmail.com>
2017-07-23 16:00:05 -0500

    Update for JSON
    - Changes RGB order of LED's
    - updates default mode
    - adds setColor function

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,9 @@
 #include "FastLED.h"
 #include <EEPROM.h>
+#include <ArduinoJson.h>
+
+
+
 // How many leds in your strip?
 #define NUM_LEDS 88
 
@@ -11,7 +15,7 @@
 
 
 #define LED_TYPE    WS2812 // Any LEDS's that FastLED supports.
-#define COLOR_ORDER GRB // LED Order (If your colors are wrong, flip this around.)
+#define COLOR_ORDER BRG // LED Order (If your colors are wrong, flip this around.)
 
 #define DEBUG true //
 
@@ -34,6 +38,7 @@ struct setting_t {
   String mode;
 } settings;
 
+
 // Do some setup.
 void setup() {
   Serial.setTimeout(250); // Set Serial timeout (increase for slower Arduino's)
@@ -41,11 +46,23 @@ void setup() {
 
   // setup defaults for settings.
   settings.color = {10, 100, 255};
-  settings.mode = "solidrainbow";
+  settings.mode = "solid";
 
   LEDS.addLeds<LED_TYPE, DATA_PIN, COLOR_ORDER>(leds, NUM_LEDS);
   LEDS.setBrightness(50);
   Serial.println("Ready");
+}
+
+
+void setColor(String json)
+{
+    DynamicJsonBuffer jsonBuffer;
+    JsonObject& root = jsonBuffer.parseObject(json);
+    if (!root.success()){
+      Serial.println("parseObject() failed");
+      return;
+    }
+    settings.color = {root["r"], root["g"], root["b"]};
 }
 
 /**
@@ -64,6 +81,7 @@ void parseSerial(String input) {
   if (input.indexOf('color') > -1) {
     input.trim(); //removes whitespace
     input.remove(0, 6);
+    setColor(input);
   }
 }
 
@@ -90,11 +108,11 @@ void solidColor() {
 
 void solidRainbow() {
   static uint8_t hue = 0;
-	// For each LED
-	for(int i = 0; i < NUM_LEDS; i++) {
-		// Set the i'th led to hue
-		leds[i] = CHSV(hue, 255, 255);
-	}
+  // For each LED
+  for(int i = 0; i < NUM_LEDS; i++) {
+    // Set the i'th led to hue
+    leds[i] = CHSV(hue, 255, 255);
+  }
   // Show the leds
   FastLED.show();
   // Change hue
@@ -111,32 +129,32 @@ void fadeall() { // Fades LED's over time.
 
 void circleColor() {
   for(int i = 0; i < NUM_LEDS; i++) {
-		// Set the i'th led to COLOR
-		leds[i] = CRGB(settings.color.red, settings.color.blue, settings.color.green);
-		// Show the leds
-		FastLED.show();
-		// now that we've shown the leds, reset the i'th led to black
-		// leds[i] = CRGB::Black;
-		fadeall();
-		// Wait a little bit before we loop around and do it again
-		delay(10);
-	}
+    // Set the i'th led to COLOR
+    leds[i] = CRGB(settings.color.red, settings.color.blue, settings.color.green);
+    // Show the leds
+    FastLED.show();
+    // now that we've shown the leds, reset the i'th led to black
+    // leds[i] = CRGB::Black;
+    fadeall();
+    // Wait a little bit before we loop around and do it again
+    delay(10);
+  }
 }
 
 void circleRainbow() {
   static uint8_t hue = 0;
-	// First slide the led in one direction
-	for(int i = 0; i < NUM_LEDS; i++) {
-		// Set the i'th led to red
-		leds[i] = CHSV(hue++, 255, 255);
-		// Show the leds
-		FastLED.show();
-		// now that we've shown the leds, reset the i'th led to black
-		// leds[i] = CRGB::Black;
-		fadeall();
-		// Wait a little bit before we loop around and do it again
-		delay(10);
-	}
+  // First slide the led in one direction
+  for(int i = 0; i < NUM_LEDS; i++) {
+    // Set the i'th led to red
+    leds[i] = CHSV(hue++, 255, 255);
+    // Show the leds
+    FastLED.show();
+    // now that we've shown the leds, reset the i'th led to black
+    // leds[i] = CRGB::Black;
+    fadeall();
+    // Wait a little bit before we loop around and do it again
+    delay(10);
+  }
 }
 
 void loop() {
